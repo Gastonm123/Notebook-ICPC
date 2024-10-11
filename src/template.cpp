5bb6711614db03fc6b27f45ffbcf908587714e6d
Milagros Osimi <milagrososimi@gmail.com>
2024-10-11 17:53:39 -0300

    cambio template

--- src/template.cpp
+++ src/template.cpp
@@ -3,10 +3,10 @@ using namespace std;
 #define dprint(v) cout << #v "=" << v << endl
 #define forr(i, a, b) for (int i = (a); i < (b); i++)
 #define forn(i, n) forr(i, 0, n)
-#define dforn(i, n) for (int i = n - 1; i >= 0; i--)
+#define dforr(i, a, b) for (int i = (b - 1); i >= (a); i--)
+#define dforn(i, n) dforr(i,0,n)
 #define forall(it, v) for (auto it = v.begin(); it != v.end(); ++it)
 #define sz(c) ((int)c.size())
-#define zero(v) memset(v, 0, sizeof(v))
 #define pb push_back
 #define fst first
 #define snd second
@@ -14,11 +14,9 @@ using namespace std;
 #define all(v) begin(v),end(v)
 #define endl '\n'
 typedef long long ll;
-typedef pair<int, int> ii;
-typedef vector<int> vi;
+typedef pair<int, int> pii;
 
 int main(int argc, char **argv){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     if(argc == 2) freopen(argv[1], "r", stdin);
-
 }
