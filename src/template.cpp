76c1eac3f24d09a2711dddcd8422f685d405bc1e
Mili y Fede <milagrososimi@gmail.com>
2024-10-14 15:53:45 -0300

    cambio template

--- src/template.cpp
+++ src/template.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define dprint(v) cout << #v "=" << v << endl
-#define forr(i, a, b) for (int i = (a); i < (b); i++)
+
+#define forr(i, a, b) for (int i = int(a); i < int(b); i++)
 #define forn(i, n) forr(i, 0, n)
-#define dforr(i, a, b) for (int i = (b - 1); i >= (a); i--)
+#define dforr(i, a, b) for (int i = int(b)-1; i >= int(a); i--)
 #define dforn(i, n) dforr(i,0,n)
-#define forall(it, v) for (auto it = v.begin(); it != v.end(); ++it)
-#define sz(c) ((int)c.size())
+#define all(v) begin(v),end(v)
+#define sz(v) (int(size(v)))
 #define pb push_back
 #define fst first
 #define snd second
 #define mp make_pair
-#define all(v) begin(v),end(v)
 #define endl '\n'
+#define dprint(v) cout << #v "=" << v << endl
+
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int main(int argc, char **argv){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    if(argc == 2) freopen(argv[1], "r", stdin);
-
-    
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
 }
