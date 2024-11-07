372744c41249bc9ee064fe61f16d67fa9a5b63b1
Milagros Osimi <milagrososimi@gmail.com>
2024-11-07 14:35:41 -0300

    stirling y bell

---
+++ src/matematica/stirling_bell.cpp
@@ -0,0 +1,13 @@
+ll STR[MAXN][MAXN], Bell[MAXN];
+
+forr(i, 1, MAXN)STR[i][1] = 1;
+forr(i, 2, MAXN)STR[1][i] = 0;
+forr(i, 2, MAXN)forr(j, 2, MAXN){
+    STR[i][j] = (STR[i-1][j-1] + j*STR[i-1][j]%MOD) %MOD;  
+}
+forn(i, MAXN){
+    Bell[i] = 0;
+    forn(j, MAXN){
+        Bell[i] = (Bell[i] + STR[i][j]) %MOD;
+    }
+}
