04afa85d55194997105decc4e74edf962cf407b0
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2025-11-01 21:14:10 +0000

    Agrega uniform_real_distribution

--- src/otros/random.cpp
+++ src/otros/random.cpp
@@ -1,3 +1,6 @@
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 mt19937 generator(seed);
 generator(); // generar un numero aleatorio entre 0 y 4294967295
+default_random_engine engine;
+uniform_real_distribution dist(0.0, 1.0);
+dist(engine); // generar un numero entre 0 y 1 con prob uniforme
