62711aa4aa658ef8e0e0fe9f2f0a77a5898a9c9a
Federico Bersano <bersanofede@gmail.com>
2025-11-04 19:19:05 -0300

    Actualizar generación de números aleatorios

--- src/otros/random.cpp
+++ src/otros/random.cpp
@@ -1,6 +1,18 @@
-unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
 mt19937 generator(seed);
-generator(); // generar un numero aleatorio entre 0 y 4294967295
-default_random_engine engine;
-uniform_real_distribution dist(0.0, 1.0);
-dist(engine); // generar un numero entre 0 y 1 con prob uniforme
+generator(); // generar un número aleatorio entre 0 y 4294967295
+// existe mt19937_64 para la versión de 64 bits, que probablemente sea más rápido
+
+/*
+// también se puede hacer lo siguiente para una versión hasta 3x más rápida:
+#include <ext/random>
+using namespace __gnu_cxx;
+unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+sfmt19937 generator(seed); // existe también sfmt19937_64
+*/
+
+uniform_int_distribution<ll> dist_int(L, R);
+dist_int(generator); // generar un entero en [L, R] (cerrado-cerrado) con prob uniforme
+
+uniform_real_distribution<double> dist_real(0.0, 1.0);
+dist_real(generator); // generar un real en [0, 1) (cerrado-abierto) con prob uniforme
