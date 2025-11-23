79b857f8b400f9a3c7741e5145e9a1ccdd90c124
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2025-11-23 00:02:00 +0000

    Mejor version de centroid

--- src/grafos/centroid.cpp
+++ src/grafos/centroid.cpp
@@ -1,42 +1,28 @@
-bool vis[MAXN]; //para centroides
-vector<int> g[MAXN]; int size[MAXN];
-vector<int> g1[MAXN]; //para centroides
+int sz[MAXN], ft[MAXN], tk[MAXN];
 void calcsz(int u, int p) {
-    size[u] = 1;
-    for (int v : g[u]) if (v != p && !vis[v]) {
-        calcsz(v, u); size[u] += size[v]; }
-}
-int cendfs(int u, int p, int ts) {
-    int maximo = 0, pesado, r;
-    for (int v : g[u]) if (v != p && !vis[v]) {
-        if (maximo < size[v]) {
-            maximo = size[v]; pesado = v; }
+    sz[u] = 1;
+    for (auto v : g[u]) if (v!=p && !tk[v]) {
+        calcsz(v, u);
+        sz[u]+=sz[v];
     }
-    if (maximo <= (ts/2)) {
-        vis[u] = true;
-        for (int v : g[u]) if (!vis[v]) {
-            if (v == p) calcsz(v, u);
-            r = cendfs(v, u, hijos[v]);
-            add_edge(g1, u, r);
+}
+int dfs(int u, int p) {
+    int pesado = -1;
+    for (auto v : g[u]) if (v!=p && !tk[v])  {
+        if (pesado==-1 || sz[pesado]<sz[v]) pesado = v;
+    }
+    if (pesado==-1) return u;
+    if (sz[pesado]<=sz[u]/2) {
+        tk[u] = true;
+        for (auto v : g[u]) if (!tk[v]) {
+            int c=dfs(v, u);
+            ft[c]=u;
         }
-        r = u;
+        return u;
+    } else {
+        int sz_pesado=sz[pesado];
+        sz[pesado]=sz[u];
+        sz[u]-=sz_pesado;
+        return dfs(pesado, u);
     }
-    else r = cendfs(pesado, u, ts);
-    return r;
-}
-// euler para responder en el arbol de centroides
-int te[MAXN], ts[MAXN]; vector<Partial> euler;
-void do_euler(int u, int p, Partial &p) {
-    te[u] = sz(euler); euler.pb(c);
-    for (int v : g[u]) if (v != p && !vis[v]) {
-        do_euler(v, u, p); } //cambiar p
-    ts[u] = sz(euler);
-}
-Sol oncen(int u, int p) {
-    do_euler(u, p, Partial{});
-    vis[u] = true; //no tocar visitados
-    Sol r{};
-    for (int v : g1[u]) if (v != p) {
-        r = max(r, oncen(v, u)); }
-    return r;
 }
