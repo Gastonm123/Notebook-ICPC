45110b5800228fca9382f9bcab86deaf39573165
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2026-03-02 23:24:11 +0000

    Formulas, cht de don gato

--- src/estructuras/chull_trick.cpp
+++ src/estructuras/chull_trick.cpp
@@ -32,3 +32,19 @@ struct CHT { // for minimum (for maximum just change the sign of lines)
 		return c[pos].m*x+c[pos].h;
 	}
 };
+
+struct line { int a, b; }; // y = ax + b
+vector<line> cht(vector<line> a) {
+	sort(all(a), [](line x, line y) {
+		return make_pair(x.a, x.b) < make_pair(y.a, y.b); });
+	vector<line> b = {a[0]};
+	forr(i, 1, sz(a)) { line z = a[i];
+		if (b.back().a == z.a) b.pop_back();
+		while (sz(b) >= 2) { line x = b[sz(b)-2], y = b[sz(b)-1];
+			if (ll(x.b-y.b)*(z.a-x.a) < ll(x.b-z.b)*(y.a-x.a)) break;
+			b.pop_back();
+		}
+		b.pb(z);
+	}
+	return b;
+}
