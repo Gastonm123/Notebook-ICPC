6ad3aacb5da0bf464ad4c9dd2533b32138f1c475
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2026-05-24 23:44:32 +0000

    Segtree lazy de rusia

--- src/estructuras/segment_tree_lazy.cpp
+++ src/estructuras/segment_tree_lazy.cpp
@@ -1,14 +1,13 @@
 //Dado un arreglo y una operacion asociativa con neutro, get(i, j) opera sobre el rango [i, j).
 typedef int Elem;//Elem de los elementos del arreglo
 typedef int Alt;//Elem de la alteracion
-#define oper(x,y) x+y
-#define oper2(k,a,b) k*(b-a)//Aplicar actualizacion sobre [a, b)
+#define oper(x,y) (x+y)
 const Elem neutro=0; const Alt neutro2=-1;
 struct RMQ{
 	int sz;
 	Elem t[4*MAXN];
 	Alt dirty[4*MAXN];//las alteraciones pueden ser distintas a Elem
-	Elem &operator[](int p){return t[sz+p];}
+	Elem operator[](int p){return get(p,p+1);}
 	void init(int n){//O(nlgn)
 		sz = 1 << (32-__builtin_clz(n));
 		forn(i, 2*sz) t[i]=neutro;
@@ -16,17 +15,20 @@ struct RMQ{
 	}
 	void push(int n, int a, int b){//propaga el dirty a sus hijos
 		if(dirty[n]!=neutro2){
-			t[n]+=oper2(dirty[n], a, b);//altera el nodo 
-			if(n<sz){//cambiar segun el problema
-				dirty[2*n]   = dirty[n];
+			if(n<sz){
+				// CAMBIAR: como aplicar una modificacion
+				t[2*n] = dirty[n];
+				t[2*n+1] = dirty[n];
+				// CAMBIAR: como combinar dirtys
+				dirty[2*n] = dirty[n];
 				dirty[2*n+1] = dirty[n];
 			}
 			dirty[n]=neutro2;
 		}
 	}
 	Elem get(int i, int j, int n, int a, int b){//O(lgn)
-		if(j<=a || i>=b) return neutro;
 		push(n, a, b);
+		if(j<=a || i>=b) return 0;
 		if(i<=a && b<=j) return t[n];
 		int c=(a+b)/2;
 		return oper(get(i, j, 2*n, a, c), get(i, j, 2*n+1, c, b));
@@ -37,8 +39,9 @@ struct RMQ{
 		push(n, a, b);
 		if(j<=a || i>=b) return;
 		if(i<=a && b<=j){
-			dirty[n]+=val;
-			push(n, a, b);
+			// CAMBIAR: como aplicar una modificacion
+			t[n] = val; // cambiar arbol
+			dirty[n] = val; // lazy
 			return;
 		}
 		int c=(a+b)/2;
@@ -47,4 +50,4 @@ struct RMQ{
 		t[n]=oper(t[2*n], t[2*n+1]);
 	}
 	void alterar(Alt val, int i, int j){alterar(val,i,j,1,0,sz);}
-}rmq;
+} rmq;
